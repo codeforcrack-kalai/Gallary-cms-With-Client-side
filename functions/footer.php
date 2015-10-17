2241081f27746319d09110970f22a55483311383
kalaiselva <codeforcrack@gmail.com>
2015-10-17 07:45:10 -0700

    Create footer.php

---
+++ functions/footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid" style=" background-color:#000; " >
+<div class="container" style=" background-color:#000; ">
+<footer>
+  <p>&copy; <a href="https://www.linkedin.com/in/kalaiselva"  target="_blank">Created by kalaiselva </a>a free lance developer</p>
+</footer>
+
+
+</div>
+</div>
