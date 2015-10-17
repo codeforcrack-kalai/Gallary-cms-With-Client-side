88d7f01f713b3225f7c19bd4a9837b96c2c53717
kalaiselva <codeforcrack@gmail.com>
2015-10-17 07:46:10 -0700

    Create functions.php

---
+++ functions/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+
+function get_header()
+{
+
+include('header.php');
+
+}
+
+function get_footer()
+{
+
+include('footer.php');
+
+}
+
+
+function get_album_navigation()
+{
+	
+	include('album_navigation.php');
+}
+
+function get_main_gallary()
+{
+	
+		include('album_main_gallary.php');
+}
+
+function album_navigation_album_counts()
+{
+	
+	
+}
+
+
+?>
