6f5d9246cfc3a49bcf200cdea47ac3d1fd9e9f3a
kalaiselva <codeforcrack@gmail.com>
2015-10-17 07:28:12 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+include('functions/functions.php');
+?>
+
+
+<?php
+
+get_header();
+
+?>
+
+<div class="container-fluid" id="section1" >
+<div class="container">
+
+
+
+						
+						
+						
+
+                <div class="box-content" >
+                    
+					
+<?php 
+get_album_navigation();
+
+
+
+ ?>
+ 
+					
+	<?php
+
+	get_main_gallary();
+	?>
+                </div>
+            </div>
+        </div>
+		 </div>
+        <!--/span-->
+		
+		
+				<div class="container-fluid"style="height:83px;"  >
+<div class="container ">
+
+<br>
+<br><br><br><br><br><br>
+</div>
+</div>
+		
+		
+		
+		<div class="container-fluid" style=" background-image:url('img/blue.jpg'); height:345px;" >
+<div class="container" style=" background-image:url('img/blue.jpg'); ">
+
+<br>
+<br><br><br><br><br><br>
+</div>
+</div>
+
+<?php
+get_footer();
+
+?>
+
+
+    
+	<!-- external javascript -->
+
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- library for cookie management -->
+<script src="js/jquery.cookie.js"></script>
+<!-- calender plugin -->
+<script src='bower_components/moment/min/moment.min.js'></script>
+<script src='bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
+<!-- data table plugin -->
+<script src='js/jquery.dataTables.min.js'></script>
+
+<!-- select or dropdown enhancer -->
+<script src="bower_components/chosen/chosen.jquery.min.js"></script>
+<!-- plugin for gallery image view -->
+<script src="bower_components/colorbox/jquery.colorbox-min.js"></script>
+<!-- notification plugin -->
+<script src="js/jquery.noty.js"></script>
+<!-- library for making tables responsive -->
+<script src="bower_components/responsive-tables/responsive-tables.js"></script>
+<!-- tour plugin -->
+<script src="bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
+<!-- star rating plugin -->
+<script src="js/jquery.raty.min.js"></script>
+<!-- for iOS style toggle switch -->
+<script src="js/jquery.iphone.toggle.js"></script>
+<!-- autogrowing textarea plugin -->
+<script src="js/jquery.autogrow-textarea.js"></script>
+<!-- multiple file upload plugin -->
+<script src="js/jquery.uploadify-3.1.min.js"></script>
+<!-- history.js for cross-browser state change on ajax -->
+<script src="js/jquery.history.js"></script>
+<!-- application script for Charisma demo -->
+<script src="js/charisma.js"></script>
+
+
+
+</body>
+</html>
