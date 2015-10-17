583d0e18ff3f7eb357e3908d2a5b083bf83cc70a
kalaiselva <codeforcrack@gmail.com>
2015-10-17 07:47:17 -0700

    Create header.php

---
+++ functions/header.php
@@ -0,0 +1,117 @@
+<?php
+include('connection.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--
+       
+    -->
+    <meta charset="utf-8">
+    <title>Album Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+    <meta name="author" content="Muhammad Usman">
+
+    <!-- The styles -->
+    <link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
+	
+	   <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/full-slider.css" rel="stylesheet">
+		        <link href="css/kalai_custome.css" rel="stylesheet">
+
+    <link href="css/charisma-app.css" rel="stylesheet">
+    <link href='bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
+    <link href='bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
+    <link href='bower_components/chosen/chosen.min.css' rel='stylesheet'>
+    <link href='bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
+    <link href='bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='css/jquery.noty.css' rel='stylesheet'>
+    <link href='css/noty_theme_default.css' rel='stylesheet'>
+    <link href='css/elfinder.min.css' rel='stylesheet'>
+    <link href='css/elfinder.theme.css' rel='stylesheet'>
+    <link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
+    <link href='css/uploadify.css' rel='stylesheet'>
+    <link href='css/animate.min.css' rel='stylesheet'>
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+
+    <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- The fav icon -->
+
+
+</head>
+
+<body background="black">
+
+
+<div class="navbar navbar-default navbar-fixed-top navbar-transparent" style="background-image:url('img/blue.jpg');">
+				<div class="container-fluid" style=" background-image:url('img/blue.jpg'); ">
+
+	<div class="container">
+	
+	<div class="hedaer_nav_main" style=" background-image:url('img/blue.jpg'); " >
+	<div class="heder_nav_main_menu">
+	   <div class="navbar-header">
+          <a href="./" class="navbar-brand" style="color:#FFF;">Gallary</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+		
+		<div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav navbar-right" >
+		 <li>
+              <a href="#section2" style="color:#FFF;">Home</a>
+            </li>
+			
+			<li>
+              <a href="gallarycms" style="color:#FFF;">Create Albums</a>
+            </li>
+			<li>
+              <a href="gallarycms" style="color:#FFF;">Manage Albums</a>
+            </li>
+				<li>
+              <a href="#" style="color:#FFF;">About us</a>
+            </li>
+			
+	
+		  </ul>
+		  
+		  </div>
+
+
+</div>
+			</div>
+		</div>
+			</div>
+
+	</div>
+	</div>
+
+<br>
+<br>
+<?php
+/**
+
+<div class="container-fluid" style=" background-image:url('img/banner-03.jpg');height:600px;" >
+<div class="container" style="padding:122px;">
+  <div class="jumbotron" style="background-color:rgba(238, 238, 238, 0.01)">
+    <h1>My Photo Gallary!</h1>      
+    <p>Upload and Mange Your Photos and Make slideshows</p>      
+    <a href="#" class="btn btn-info btn-lg"><span></span>Upload Now</a>
+  </div>
+   </div>
+    </div>*/
+	?>
+	
